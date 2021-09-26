8da6cc9a829db1573711a344b9dc3e12205408fd
Leonardo Santos <laulaiu14@gmail.com>
2021-09-25 21:45:03 -0300

    Configurando classe firebase

---
+++ app/src/main/java/com/appgjob/teladeperguntas/FirebaseBD.java
@@ -0,0 +1,31 @@
+package com.appgjob.teladeperguntas;
+
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class FirebaseBD {
+    private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
+    public void eventoDatabse(){
+        mDatabase.child("formulario").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/com/appgjob/teladeperguntas/Formulario.java
+++ app/src/main/java/com/appgjob/teladeperguntas/Formulario.java
@@ -3,12 +3,24 @@ package com.appgjob.teladeperguntas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 public class Formulario extends AppCompatActivity {
+
+    private CheckBox  pergunta1;
+    private CheckBox  pergunta2;
+    private CheckBox  pergunta3;
+    private CheckBox  pergunta4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        pergunta1 = findViewById(R.id.chk_pergunta1);
+        pergunta2 = findViewById(R.id.chk_pergunta2);
+        pergunta3 = findViewById(R.id.chk_pergunta3);
+        pergunta4 = findViewById(R.id.chk_pergunta4);
+
     }
 }
